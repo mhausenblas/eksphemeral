523a54d56ca580794a42e0f46d32904b0c87d2d3
Michael Hausenblas <michael.hausenblas@gmail.com>
2019-06-07 19:48:03 +0100

    inits destroy function

--- svc/destroycluster/main.go
+++ svc/destroycluster/main.go
@@ -1,17 +1,44 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	_ "image/jpeg"
 	_ "image/png"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+
+	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
 )
 
+func deleteStack(name string) error {
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		return err
+	}
+	svc := cloudformation.New(cfg)
+	dsreq := svc.DeleteStackRequest(&cloudformation.DeleteStackInput{StackName: aws.String(name)})
+	resp, err := dsreq.Send(context.TODO())
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%v\n", resp.String())
+	return nil
+}
+
 func handler() error {
-	fmt.Printf("DEBUG:: destroy start\n")
-	//TODO: parse params, shell out to eksctl
-	fmt.Printf("DEBUG:: destroy done\n")
+	fmt.Printf("DEBUG:: destroy cluster start\n")
+	err := deleteStack("eksctl-eksphemeral-nodegroup-ng-df9fe94e")
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = deleteStack("eksctl-eksphemeral-cluster")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("DEBUG:: destroy cluster done\n")
 	return nil
 }
 
