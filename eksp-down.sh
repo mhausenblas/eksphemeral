f30fee2c207d35848f2bac809e04b03c0c42b1ef
Michael Hausenblas <michael.hausenblas@gmail.com>
2019-06-08 06:32:07 +0100

    improves tear-down

--- eksp-down.sh
+++ eksp-down.sh
@@ -12,7 +12,9 @@ cd svc
 make destroy
 cd ..
 
+printf "\nTear-down will complete within some 5 min. You can check the status manually, if you like, using 'make status' in the svc/ directory.\nOnce you see a message saying something like 'Stack with id eksp does not exist' you know for sure it's gone :)\n"
+
 # TBD: delete all objects in the eks-cluster-meta bucket
 # TBD: Fargate clean-up
 
-printf "Thanks for using EKSphemeral and hope to see ya soon ;)\n"
+printf "\n\nThanks for using EKSphemeral and hope to see ya soon ;)\n"
