5a050a71af542a9099dde48c8c5610bfce09becf
Michael Hausenblas <michael.hausenblas@gmail.com>
2019-06-15 16:18:23 +0100

    makes up script output nicer

--- eksp-up.sh
+++ eksp-up.sh
@@ -53,7 +53,7 @@ cd $EKSPHEMERAL_HOME/svc
 make install
 cd -
 
-printf "\nControl plane should be up now, let us verify that:\n"
+printf "\nControl plane should be up now, let us verify that: "
 
 EKSPHEMERAL_URL=$(aws cloudformation describe-stacks --stack-name eksp | jq '.Stacks[].Outputs[] | select(.OutputKey=="EKSphemeralAPIEndpoint").OutputValue' -r)
 
@@ -61,8 +61,8 @@ CONTROLPLANE_STATUS=$(curl -sL -w "%{http_code}" -o /dev/null "$EKSPHEMERAL_URL/
 
 if [ $CONTROLPLANE_STATUS == "200" ]
 then
-    printf "\nAll good, ready to launch ephemeral clusters now!\n"
+    printf " All good, ready to launch ephemeral clusters now!\n"
 else 
-    printf "\nThere was an issue setting up the EKSphemeral control plane, check the CloudFormation logs :(\n"
+    printf " There was an issue setting up the EKSphemeral control plane, check the CloudFormation logs :(\n"
     exit 1
 fi
