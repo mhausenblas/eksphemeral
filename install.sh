c777fb976eedc8f5dabae537528e1d490486511e
Michael Hausenblas <michael.hausenblas@gmail.com>
2019-06-15 14:40:34 +0100

    inits e2e install procedure

---
+++ install.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o errtrace
+set -o pipefail
+
+###############################################################################
+### GLOBALS
+
+fargateversion=0.3.0
+ekspversion=v0.3.0
+
+unameres="$(uname -s)"
+case "${unameres}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=MacOS;;
+    *)          echo "Sorry, not a supported platform" ; exit 1
+esac
+
+
+###############################################################################
+### HELPER FUNCTIONS
+
+function installFargate() {
+  case "${machine}" in
+    Linux*)     srcURL=https://github.com/jpignata/fargate/releases/download/v$fargateversion/fargate-$fargateversion-linux-amd64.zip ;;
+    MacOS*)    srcURL=https://github.com/jpignata/fargate/releases/download/v$fargateversion/fargate-$fargateversion-darwin-amd64.zip ;;
+  esac
+  echo "Attempting to download $srcURL"
+  curl -L $srcURL -o fargate$fargateversion
+  tar xopf fargate$fargateversion
+  mv ./fargate /usr/local/bin 
+  rm fargate$fargateversion
+}
+
+func installEKSphemeralCLI() {
+  case "${machine}" in
+    Linux*)     srcURL=https://github.com/mhausenblas/eksphemeral/releases/download/$ekspversion/eksp-linux ;;
+    MacOS*)    srcURL=https://github.com/mhausenblas/eksphemeral/releases/download/$ekspversion/eksp-macos ;;
+  esac
+  echo "Attempting to download $srcURL"
+  curl -L  $srcURL -o eksp
+  chmod +x eksp
+  mv ./eksp /usr/local/bin
+}
+
+###############################################################################
+### PRE-FLIGHT CHECKS
+
+if ! [ -x "$(command -v jq)" ]
+then
+  echo "Please install jq from https://stedolan.github.io/jq/download/ and try again" >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v aws)" ]
+then
+  echo "Please install aws via https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-install.html and try again" >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v fargateother)" ]
+then
+  installFargate
+fi
+
+###############################################################################
+### MAIN
+
+# Install CLI
+installEKSphemeralCLI
+
+# Install EKSphemeral control plane
+eksp install
+
+
+
