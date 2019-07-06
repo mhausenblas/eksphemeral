55b0f3d21cc24343b6a029219219b032d96befc7
Michael Hausenblas <michael.hausenblas@gmail.com>
2019-07-06 12:49:20 +0100

    adds App Mesh, Prometheus, and Grafana to deluxe

--- deluxe-install.sh
+++ deluxe-install.sh
@@ -28,19 +28,28 @@ kubectl wait nodes --for=condition=Ready --timeout=300s --all
 ################################################################################
 # ADDONS install
 
+######
 # install ArgoCD based off of:
 # https://argoproj.github.io/argo-cd/getting_started/
 echo "Now installing ArgoCD ..."
 kubectl create namespace argocd
 kubectl -n argocd apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
 # OPTION: explose the UI via ...
 # kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+
 # OPTON: log in via ...
 # kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2
 
-# install App Mesh and o11y components (Prometheus, Grafana, X-Ray)
-# based off of: https://github.com/PaulMaddox/aws-appmesh-helm
+######
+# install App Mesh and o11y components (Prometheus, Grafana, X-Ray) using:
+# https://github.com/PaulMaddox/aws-appmesh-helm
+kubectl apply -f https://raw.githubusercontent.com/PaulMaddox/aws-appmesh-helm/master/scripts/helm-rbac.yaml
+helm init --service-account tiller
+helm install -n aws-appmesh --namespace appmesh-system https://github.com/PaulMaddox/aws-appmesh-helm/releases/latest/download/aws-appmesh.tgz
 
+
+######
 # install the default Kube dashboard based off of:
 # https://docs.aws.amazon.com/eks/latest/userguide/dashboard-tutorial.html
 
